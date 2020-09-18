a17e5bc93730751ca7d5616df9d14ba0a638b615
s4nt14go <santiagopergamo@gmail.com>
2020-09-18 03:31:55 -0300

    Material UI example

--- src/main/table/Example1.js
+++ src/main/table/Example1.js
@@ -1,76 +1,53 @@
 import React from 'react'
-import styled from 'styled-components'
+
+import MaUTable from '@material-ui/core/Table'
+import TableBody from '@material-ui/core/TableBody'
+import TableCell from '@material-ui/core/TableCell'
+import TableHead from '@material-ui/core/TableHead'
+import TableRow from '@material-ui/core/TableRow'
+
 import { useTable } from 'react-table'
 
 import makeData from './makeData'
 
-const Styles = styled.div`
-  padding: 1rem;
-
-  table {
-    border-spacing: 0;
-    border: 1px solid black;
-
-    tr {
-      :last-child {
-        td {
-          border-bottom: 0;
-        }
-      }
-    }
-
-    th,
-    td {
-      margin: 0;
-      padding: 0.5rem;
-      border-bottom: 1px solid black;
-      border-right: 1px solid black;
-
-      :last-child {
-        border-right: 0;
-      }
-    }
-  }
-`
-
 function Table({ columns, data }) {
   // Use the state and functions returned from useTable to build your UI
-  const {
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    rows,
-    prepareRow,
-  } = useTable({
+  const { getTableProps, headerGroups, rows, prepareRow } = useTable({
     columns,
     data,
   })
 
   // Render the UI for your table
   return (
-    <table {...getTableProps()}>
-      <thead>
+    <MaUTable {...getTableProps()}>
+      <TableHead>
       {headerGroups.map(headerGroup => (
-        <tr {...headerGroup.getHeaderGroupProps()}>
+        <TableRow {...headerGroup.getHeaderGroupProps()}>
           {headerGroup.headers.map(column => (
-            <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+            <TableCell {...column.getHeaderProps()}>
+              {column.render('Header')}
+            </TableCell>
           ))}
-        </tr>
+        </TableRow>
       ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
+      </TableHead>
+      <TableBody>
       {rows.map((row, i) => {
         prepareRow(row)
         return (
-          <tr {...row.getRowProps()}>
+          <TableRow {...row.getRowProps()}>
             {row.cells.map(cell => {
-              return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+              return (
+                <TableCell {...cell.getCellProps()}>
+                  {cell.render('Cell')}
+                </TableCell>
+              )
             })}
-          </tr>
+          </TableRow>
         )
       })}
-      </tbody>
-    </table>
+      </TableBody>
+    </MaUTable>
   )
 }
 
@@ -118,9 +95,9 @@ function App() {
   const data = React.useMemo(() => makeData(20), [])
 
   return (
-    <Styles>
+    <div>
       <Table columns={columns} data={data} />
-    </Styles>
+    </div>
   )
 }
 
