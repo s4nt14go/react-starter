beb1e362344e6a68e58d651e6dedb8e5a6c3a50a
s4nt14go <santiagopergamo@gmail.com>
2020-09-18 02:52:08 -0300

    Row Selection and Pagination example

--- src/main/table/Example1.js
+++ src/main/table/Example1.js
@@ -1,9 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
-import { useTable, useRowSelect } from 'react-table'
+import { useTable, usePagination, useRowSelect } from 'react-table'
 
 import makeData from './makeData'
-import {log} from '../../service/util';
 
 const Styles = styled.div`
   padding: 1rem;
@@ -32,6 +31,10 @@ const Styles = styled.div`
       }
     }
   }
+
+  .pagination {
+    padding: 0.5rem;
+  }
 `
 
 const IndeterminateCheckbox = React.forwardRef(
@@ -57,28 +60,38 @@ function Table({ columns, data }) {
     getTableProps,
     getTableBodyProps,
     headerGroups,
-    rows,
     prepareRow,
+    page, // Instead of using 'rows', we'll use page,
+    // which has only the rows for the active page
+
+    // The rest of these things are super handy, too ;)
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
     selectedFlatRows,
-    state: { selectedRowIds },
+    state: { pageIndex, pageSize, selectedRowIds },
   } = useTable(
     {
       columns,
       data,
     },
+    usePagination,
     useRowSelect,
     hooks => {
-      log({hooks});
-      console.log('hooks.visibleColumns', hooks.visibleColumns);
       hooks.visibleColumns.push(columns => [
         // Let's make a column for selection
         {
           id: 'selection',
           // The header can use the table's getToggleAllRowsSelectedProps method
           // to render a checkbox
-          Header: ({ getToggleAllRowsSelectedProps }) => (
+          Header: ({ getToggleAllPageRowsSelectedProps }) => (
             <div>
-              <IndeterminateCheckbox {...getToggleAllRowsSelectedProps()} />
+              <IndeterminateCheckbox {...getToggleAllPageRowsSelectedProps()} />
             </div>
           ),
           // The cell can use the individual row's getToggleRowSelectedProps method
@@ -97,6 +110,21 @@ function Table({ columns, data }) {
   // Render the UI for your table
   return (
     <>
+      <pre>
+        <code>
+          {JSON.stringify(
+            {
+              pageIndex,
+              pageSize,
+              pageCount,
+              canNextPage,
+              canPreviousPage,
+            },
+            null,
+            2
+          )}
+        </code>
+      </pre>
       <table {...getTableProps()}>
         <thead>
         {headerGroups.map(headerGroup => (
@@ -108,7 +136,7 @@ function Table({ columns, data }) {
         ))}
         </thead>
         <tbody {...getTableBodyProps()}>
-        {rows.slice(0, 10).map((row, i) => {
+        {page.map((row, i) => {
           prepareRow(row)
           return (
             <tr {...row.getRowProps()}>
@@ -120,21 +148,68 @@ function Table({ columns, data }) {
         })}
         </tbody>
       </table>
-      <p>Selected Rows: {Object.keys(selectedRowIds).length}</p>
-      <pre>
-        <code>
-          {JSON.stringify(
-            {
-              selectedRowIds: selectedRowIds,
-              'selectedFlatRows[].original': selectedFlatRows.map(
-                d => d.original
-              ),
-            },
-            null,
-            2
-          )}
-        </code>
-      </pre>
+      {/*
+        Pagination can be built however you'd like.
+        This is just a very basic UI implementation:
+      */}
+      <div className="pagination">
+        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+          {'<<'}
+        </button>{' '}
+        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+          {'<'}
+        </button>{' '}
+        <button onClick={() => nextPage()} disabled={!canNextPage}>
+          {'>'}
+        </button>{' '}
+        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+          {'>>'}
+        </button>{' '}
+        <span>
+          Page{' '}
+          <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>{' '}
+        </span>
+        <span>
+          | Go to page:{' '}
+          <input
+            type="number"
+            defaultValue={pageIndex + 1}
+            onChange={e => {
+              const page = e.target.value ? Number(e.target.value) - 1 : 0
+              gotoPage(page)
+            }}
+            style={{ width: '100px' }}
+          />
+        </span>{' '}
+        <select
+          value={pageSize}
+          onChange={e => {
+            setPageSize(Number(e.target.value))
+          }}
+        >
+          {[10, 20, 30, 40, 50].map(pageSize => (
+            <option key={pageSize} value={pageSize}>
+              Show {pageSize}
+            </option>
+          ))}
+        </select>
+        <pre>
+          <code>
+            {JSON.stringify(
+              {
+                selectedRowIds: selectedRowIds,
+                'selectedFlatRows[].original': selectedFlatRows.map(
+                  d => d.original
+                ),
+              },
+              null,
+              2
+            )}
+          </code>
+        </pre>
+      </div>
     </>
   )
 }
@@ -180,8 +255,7 @@ function App() {
     []
   )
 
-  const data = React.useMemo(() => makeData(10, 3), [])
-  //log({data});
+  const data = React.useMemo(() => makeData(10000), [])
 
   return (
     <Styles>
